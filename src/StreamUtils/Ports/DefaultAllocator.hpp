b2ea5bb69f57d542b5ae9abaef37b0428e9aae29
connie talbot <mhmtsui@connect.hku.hk>
2021-06-18 20:17:55 +0800

    <feat> use static mem instead

--- src/StreamUtils/Ports/DefaultAllocator.hpp
+++ src/StreamUtils/Ports/DefaultAllocator.hpp
@@ -7,14 +7,28 @@
 namespace StreamUtils {
 
 #include <stdlib.h>  // malloc, free, size_t
+#define USE_STATIC_MEM
+#define STATIC_SIZE 256
+static char _static_buf[STATIC_SIZE+1];
+//#include <stdlib.h>  // malloc, free, size_t
 
 struct DefaultAllocator {
   void* allocate(size_t n) {
+#ifdef USE_STATIC_MEM
+    if (n > STATIC_SIZE){
+      return NULL;
+    }else{
+      return _static_buf;
+    }
+#else
     return malloc(n);
+#endif
   }
 
   void deallocate(void* p) {
+#ifndef USE_STATIC_MEM    
     free(p);
+#endif
   }
 };
 
